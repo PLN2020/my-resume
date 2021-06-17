758b39c54fbc7b9559250e7ae33e736e46a04a7d
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-06-16 23:49:40 -0400

    created edit form and added back link on post show views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   
     def update
       @post.update(post_params)
-      redirect_to posts_path(@post)
+      redirect_to post_path(@post)
       authorize @post
     end
   
